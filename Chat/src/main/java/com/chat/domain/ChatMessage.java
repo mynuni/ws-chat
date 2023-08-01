8227649da2f082b0ee7e82e9acdca0e780d5842e
mynuni <elelaka0000@gmail.com>
2023-08-01 17:01:16 +0900

    feat: 채팅 내역 저장을 위한 엔티티 추가 (#2)

--- Chat/src/main/java/com/chat/domain/ChatMessage.java
+++ Chat/src/main/java/com/chat/domain/ChatMessage.java
@@ -1,5 +1,17 @@
 package com.chat.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,12 +21,30 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "CHAT_MESSAGE")
 public class ChatMessage {
-	private MessageType type;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(length = 50, nullable = false)
 	private String sender;
+
+	@Column(length = 500, nullable = false)
 	private String content;
+
+	@Column(length = 20, nullable = false)
+	@Enumerated(EnumType.STRING)
+	private MessageType type;
 
 	public enum MessageType {
 		CHAT, JOIN, LEAVE
 	}
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chatroom_id", nullable = false)
+    private ChatRoom chatRoom;
+	
 }
--- Chat/src/main/java/com/chat/domain/ChatRoom.java
+++ Chat/src/main/java/com/chat/domain/ChatRoom.java
@@ -1,5 +1,20 @@
 package com.chat.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,10 +22,21 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@Table(name = "CHAT_ROOM")
+@Entity
 public class ChatRoom {
-	private int roomId;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long roomId;
+	
+	@Column(name = "CHAT_ROOM_NAME", length = 50, nullable = false)
 	private String chatRoomName;
-	private String socketSession;
+	
+	@Column(name = "VISITOR_COUNT")
 	private int visitorCount;
+	
+	@OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<ChatMessage> messages = new ArrayList<>();
 
 }
