77cd947697bab54cf980aac30a2bac389608554d
mynuni <elelaka0000@gmail.com>
2023-08-16 11:46:23 +0900

    refactor: 참여자 관리 레포지토리 추상화

---
+++ Chat/src/main/java/com/chat/service/MapParticipantManager.java
@@ -0,0 +1,44 @@
+package com.chat.service;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+//@Component
+public class MapParticipantManager implements ParticipantManager {
+
+	private final ConcurrentHashMap<Long, Set<String>> participants = new ConcurrentHashMap<>();
+	private final SimpMessageSendingOperations messagingTemplate;
+
+	public Set<String> getParticipants(Long roomId) {
+		return participants.getOrDefault(roomId, new HashSet<>());
+	}
+
+	public void addParticipant(Long roomId, String username) {
+		participants.computeIfAbsent(roomId, key -> new HashSet<>()).add(username);
+	}
+
+	public void sendParticipantsUpdate(Long roomId, Set<String> participants) {
+		messagingTemplate.convertAndSend("/topic/participants/" + roomId, participants);
+	}
+
+	public void removeParticipant(Long roomId, String username) {
+		participants.computeIfPresent(roomId, (key, value) -> {
+			value.remove(username);
+
+			if (value.isEmpty()) {
+				participants.remove(roomId);
+				return null;
+			} else {
+				return value;
+			}
+		});
+	}
+
+}
--- Chat/src/main/java/com/chat/service/ParticipantManager.java
+++ Chat/src/main/java/com/chat/service/ParticipantManager.java
@@ -1,44 +1,15 @@
 package com.chat.service;
 
-import java.util.HashSet;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.stereotype.Component;
+public interface ParticipantManager {
 
-import lombok.RequiredArgsConstructor;
+	Set<String> getParticipants(Long roomId);
 
-@RequiredArgsConstructor
-@Component
-public class ParticipantManager {
+	void addParticipant(Long roomId, String username);
 
-	private final ConcurrentHashMap<Long, Set<String>> participants = new ConcurrentHashMap<>();
-	private final SimpMessageSendingOperations messagingTemplate;
+	void sendParticipantsUpdate(Long roomId, Set<String> participants);
 
-	public Set<String> getParticipants(Long roomId) {
-		return participants.getOrDefault(roomId, new HashSet<>());
-	}
-
-	public void addParticipant(Long roomId, String username) {
-		participants.computeIfAbsent(roomId, key -> new HashSet<>()).add(username);
-	}
-
-	public void sendParticipantsUpdate(Long roomId, Set<String> participants) {
-		messagingTemplate.convertAndSend("/topic/participants/" + roomId, participants);
-	}
-
-	public void removeParticipant(Long roomId, String username) {
-		participants.computeIfPresent(roomId, (key, value) -> {
-			value.remove(username);
-
-			if (value.isEmpty()) {
-				participants.remove(roomId);
-				return null;
-			} else {
-				return value;
-			}
-		});
-	}
+	void removeParticipant(Long roomId, String username);
 
 }
